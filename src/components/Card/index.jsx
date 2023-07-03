746eb18413a7a063aea7bc4b89b08c3b66c998d1
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-07-03 09:20:50 -0500

    consumiendo fakestore

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card = () => {
+const Card = (producto) => {
   return (
     <div className='bg-white cursor-pointer w-56 h-60'>
         <figure className='relative mb-2 w-full h-4/5'>
-            <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-1 p-1'>Electronics</span>
-            <img className="w-full h-full object-cover rounded-lg" src='https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=""/>
+            <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-1 p-1'>{producto.producto.category.name}</span>
+            <img className="w-full h-full object-cover rounded-lg" src={producto.producto.images[0]} alt=""/>
             <div className='absolute top-0 right-0 flex items-center justify-center bg-white w-6 h-6 rounded-full m-1'>+</div>
         </figure>
         <p className="flex justify-between"> 
-            <span className="text-sm font-light">Headphones</span>
-            <span className="text-lg font-semibold">$300</span>
+            <span className="text-sm font-light">{producto.producto.title}</span>
+            <span className="text-lg font-semibold">${producto.producto.price}</span>
         </p>
     </div>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from "react"
 import Card from "../../components/Card"
 import Layout from "../../components/Layout"
 
-function Home(){
-    return(
-       <Layout>
-           <h1>Home</h1>
-           <Card/>
-       </Layout>
+function Home() {
+    const [products, setProducts] = useState(null)
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <Layout>
+            <h1>Home</h1>
+            <div className="grid grid-cols-4 w-full max-w-screen-lg">
+                {
+                    products?.map(product => (
+                        <Card key={product.id} producto={product} />
+                    ))
+                }
+            </div>
+        </Layout>
     )
 }
 export default Home
