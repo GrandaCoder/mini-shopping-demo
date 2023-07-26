f53e806c70752bcea9a52fc65f846dec6b34f9c6
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-07-26 10:42:16 -0500

    ADDED: estilos al sign in

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,25 @@
 import Layout from "../../components/Layout"
 
-function SignIn(){
-    return(
+function SignIn() {
+    return (
         <Layout>
-            <h1>SignIn</h1>
+            <div className="min-w-[500px] ">
+                <h1 className="text-4xl text-center">Bienvenido</h1>
+                <form action="" className="flex flex-col m-4 p-3 ">
+                    <div className="mb-3 w-full flex ">
+                        <label className="min-w-[100px]" htmlFor="email">Email:</label>
+                        <input className="w-full pl-1" type="email" id="email" placeholder="example@mail.com" required />
+                    </div>
+
+                    <div className="mb-3 w-full flex">
+                        <label className="min-w-[100px]" htmlFor="password">Password:</label>
+                        <input className="w-full pl-1" type="password" id="password" placeholder="******" required />
+                    </div>
+                    <button className="my-5 w-full p-3 bg-black text-white rounded-lg cursor-pointer">Log in</button>
+                    <p className=" mb-2 text-center underline cursor-pointer">¿olvíaste tu contraseña?</p>
+                    <button className=" w-full p-3 bg-gray-400 text-white rounded-lg cursor-pointer">Registrarse</button>
+                </form>
+            </div>
         </Layout>
     )
 }
