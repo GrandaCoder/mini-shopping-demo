eb9e0fca8d82c68fe8ec45e84b46f4b3b458d69b
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-07-27 09:21:46 -0500

    se corrige signin estilos

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -5,7 +5,7 @@ function SignIn() {
         <Layout>
             <div className="min-w-[500px] ">
                 <h1 className="text-4xl text-center">Bienvenido</h1>
-                <form action="" className="flex flex-col m-4 p-3 ">
+                <form action="" className="flex flex-col my-4 py-3 ">
                     <div className="mb-3 w-full flex ">
                         <label className="min-w-[100px]" htmlFor="email">Email:</label>
                         <input className="w-full pl-1" type="email" id="email" placeholder="example@mail.com" required />
@@ -16,9 +16,9 @@ function SignIn() {
                         <input className="w-full pl-1" type="password" id="password" placeholder="******" required />
                     </div>
                     <button className="my-5 w-full p-3 bg-black text-white rounded-lg cursor-pointer">Log in</button>
-                    <p className=" mb-2 text-center underline cursor-pointer">¿olvíaste tu contraseña?</p>
-                    <button className=" w-full p-3 bg-gray-400 text-white rounded-lg cursor-pointer">Registrarse</button>
                 </form>
+                <p className=" mb-2 text-center underline cursor-pointer">¿olvíaste tu contraseña?</p>
+                <button className=" w-full p-3 bg-gray-400 text-white rounded-lg cursor-pointer">Registrarse</button>
             </div>
         </Layout>
     )
