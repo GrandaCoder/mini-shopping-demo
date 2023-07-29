9738b5106a440ddfebc8fb958899f3247a4b83fb
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-07-29 09:21:38 -0500

    se agrega estilos a account

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,9 +1,16 @@
 import Layout from "../../components/Layout"
 
-function Account(){
-    return(
+function Account() {
+    return (
         <Layout>
-            <h1>Account</h1>
+            <div className="min-w-[500px] ">
+                <h1 className="text-4xl text-center">Account</h1>
+                <div className="flex flex-col my-4 py-3 ">
+                    <p className="mb-2">Name: <span className="font-medium">John Doe</span></p>
+                    <p className="mb-2">Email: <span className="font-medium">G7b7N@example.com</span></p>
+                    <button className="my-5 w-full p-3 bg-black text-white rounded-lg cursor-pointer">Edit</button>
+                </div>
+            </div>
         </Layout>
     )
 }
